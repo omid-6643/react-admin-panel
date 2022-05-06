d4e58b619937a7d461837674af3283ec17d023d5
omid beigi <o.beygi@gmail.com>
2022-05-06 18:36:22 +0430

    5/6

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import "./home.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
+import Feature from "../../components/feature/Feature";
+import Chart from "../../components/chart/Chart";
 
 const home = () => {
   return (
@@ -14,6 +16,10 @@ const home = () => {
           <Widget type="order" />
           <Widget type="earning" />
           <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Feature />
+          <Chart />
         </div>
       </div>
     </div>
